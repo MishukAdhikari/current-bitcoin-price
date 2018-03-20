7fdc3af60520ac3cad4b3a68e91b91c5fe87678a
Mishuk Adhikari <mishuk.pq.bd@gmail.com>
2018-03-21 00:06:13 +0600

    Commnet added before function.php

--- functions.php
+++ functions.php
@@ -1,13 +1,17 @@
 <?php
 
 /**
-* CBP Main Functions
-*/
+ * Current Bitcoin Price
+ *
+ * main funciton of the plugin
+ */
 class CBP_main_functions {
 	
 	public function __construct() {
+		
 		add_action( 'wp_enqueue_scripts', array( $this, 'cbp_enqueuing_javascript_file' ) );
 		add_action( 'admin_head', array( $this, 'cbp_tinnymce_button' ) );
+		
 		global $pagenow;
 		
 		if (( $pagenow == 'options-general.php' ) && ($_GET['page'] == 'cbp-admin-dashboard')) {
@@ -17,14 +21,10 @@ class CBP_main_functions {
 	}
 	
 	/**
-	 * Enqueue scripts
-	 *
-	 * @param string $handle Script name
-	 * @param string $src Script url
-	 * @param array $deps (optional) Array of script names on which this script depends
-	 * @param string|bool $ver (optional) Script version (used for cache busting), set to null to disable
-	 * @param bool $in_footer (optional) Whether to enqueue the script before </head> or before </body>
-	 */	
+	 * Adding js and css file
+	 * 
+	 * @return void
+	 */
 	public function cbp_enqueuing_javascript_file() {
 
 		$CBP_admin = new CBP_admin_area();
@@ -33,12 +33,21 @@ class CBP_main_functions {
 		wp_localize_script( 'cbp-price-main-js', 'cbp', array( 'value' => $CBP_admin->cbp_dec_option_value() ) );
 	}
 
-	// Js and css for admin dashboard
+	/**
+	 * Js and Css for backend
+	 * 
+	 * @return void
+	 */
 	public function cbp_admin_assets() {
 		wp_enqueue_script( 'cbp-admin-js', plugins_url( '/assets/js/cbp_admin.js', __FILE__ ), array( 'jquery' ), false, false );
 		wp_enqueue_style( 'cbp-admin-style', plugins_url( '/assets/css/cbp-admin.css', __FILE__ ) );
 	}
 
+	/**
+	 * TinnyMCE button
+	 * 
+	 * @return void
+	 */
 	public function cbp_tinnymce_button() {
 
 		$tinny_checkbox = get_option('cbp_options')['cbp_tinny_checkbox'];
@@ -49,7 +58,12 @@ class CBP_main_functions {
 		}
 	}
 
-	// Declare script for new button
+	/**
+	 * Declaring button
+	 * 
+	 * @param button array
+	 * @return array
+	 */
 	public function button_for_tinymce_plugin( $plugin_array ) {
 
 		$plugin_array['cbp_mce_button'] = plugins_url( '/assets/js/cbp-tinnymce-custom-button.js', __FILE__ );
@@ -57,7 +71,13 @@ class CBP_main_functions {
 		return $plugin_array;
 	}
 
-	// Register new button in the editor
+	/**
+	 * Adding tinny button
+	 * if is page
+	 * 
+	 * @param string
+	 * @return array
+	 */
 	public function register_mce_button( $buttons ) {
 		
 		if ((isset($_GET['post_type']) && $_GET['post_type'] == 'page')) :
